d6e49c6e2ab3091c3ffd10b066b8288fb5bb5d3b
hungphong0910 <phong.tran@desports.gg>
2022-10-03 14:25:17 +0700

    fix(factory): fix create pool function

--- contracts/PoolFactory.sol
+++ contracts/PoolFactory.sol
@@ -7,7 +7,7 @@ import "../contracts/SwapToken.sol";
 contract PoolFactory is Initializable {
   event SwapContractCreate(address swapContractAddress, address contractOwner, address token1Address, address token2Address);
 
-  address[] swapContracts;
+  address[] pools;
 
   mapping(address => address[]) poolOwner;
 
@@ -16,25 +16,35 @@ contract PoolFactory is Initializable {
   }
 
   function createPool(
+    uint256 _pool_id,
     address _tokenAAddress,
     string memory _tokenAName,
     string memory _tokenASymbol,
     address _tokenBAddress,
     string memory _tokenBName,
-    string memory _tokenBSymbol,
-    uint256 _salt
-  ) public {
-    require(msg.sender == tx.origin, "Cannot create pool from contract");
+    string memory _tokenBSymbol
+  ) external {
+    require(msg.sender == tx.origin, "Cannot create pool from smart contract");
 
     address payable swapContractAddress;
     bytes memory bytecode = type(SwapToken).creationCode;
+    bytes32 salt = keccak256(
+      abi.encodePacked(
+        msg.sender,
+        _pool_id,
+        block.timestamp,
+        pools.length,
+        _tokenAAddress,
+        _tokenBAddress
+      )
+    );
 
     assembly {
       swapContractAddress := create2(
         0,
         add(bytecode, 0x20),
         mload(bytecode),
-        _salt
+        salt
       )
     }
 
@@ -47,7 +57,7 @@ contract PoolFactory is Initializable {
       _tokenBSymbol
     );
 
-    swapContracts.push(swapContractAddress);
+    pools.push(swapContractAddress);
     poolOwner[msg.sender].push(swapContractAddress);
     emit SwapContractCreate(swapContractAddress, msg.sender, _tokenAAddress, _tokenBAddress);
   }
